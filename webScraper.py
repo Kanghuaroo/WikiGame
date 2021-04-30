3632757f62590f1f328f525d61787e6ad08aa88d
David Miller <5035dcmiller@gmail.com>
2021-04-29 23:53:33 -0400

    first working version done using sets; speed ups needed

--- webScraper.py
+++ webScraper.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 def getLinksOut(website):
-    res = requests.get(website)
+    urlStart = 'https://en.wikipedia.org'
+    res = requests.get(urlStart + website)
     txt = res.text
 
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -11,10 +12,69 @@ def getLinksOut(website):
     arr = []
     for link in soup.findAll('a'):
         temp = link.get('href')
-        if temp and ('https://') in temp:
-            print(link.get('href'))
-            arr.append(temp)
+        if temp and temp.startswith('/wiki/'):
+                #print(temp)
+                arr.append(temp)
+    return arr
 
-start = 'https://en.wikipedia.org/wiki/Alan_Turing'
-end = 'https://en.wikipedia.org/wiki/World_War_II'
-getLinksOut('https://webkinz.com')
+def getWikiOnlyLinks(arr):
+    special = ['Category', 'Help', '%', '#', ':', '(identifier)', 'Timeline', 'Main_Page']
+    flag = False
+    uniqueLink = set()
+
+    for link in arr:
+        for char in special:
+            if char in link:
+                flag = True
+                break
+        if flag:
+            flag = False
+            continue
+        uniqueLink.add(link)
+
+    return uniqueLink
+
+def breadthSearch(start, end, depth):
+    queue = []
+    #Path so far, next dir
+    queue.append(([start], start))
+    seen = set()
+    seen.add(start)
+   
+    while len(queue) > 0:
+        curr = queue.pop(0)
+        print("newLink = ", curr[1], 'from', curr[0])
+        for i in range(0, 3):
+            if i < len(queue):
+                print("\t", queue[i])
+        print("============================")
+        
+        nextLink = getWikiOnlyLinks(getLinksOut(curr[1]))
+        for i in nextLink:
+            #Don't do double takes
+            if i in seen:
+                continue
+            else:
+                seen.add(i)
+
+            newPath = curr[0].copy()
+            newPath.append(i)
+            #check
+            if i == end:
+                print('DONE')
+                return newPath
+
+            queue.append((newPath, i))
+
+start = '/wiki/Alan_Turing'
+#end = 'https://en.wikipedia.org/wiki/World_War_II'
+#end = '/wiki/Data'
+#end = '/wiki/Philosophy'
+end = '/wiki/Algebraic_number'
+#getWikiOnlyLinks(getLinksOut(start))
+#tmp = getLinksOut(start)
+#getWikiOnlyLinks(tmp)
+
+tmp = breadthSearch(start, end, 0)
+print("=============================")
+print(tmp)
